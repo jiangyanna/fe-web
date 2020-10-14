205ab0ea978b741d3cd5771d453b38f3bdb91ddf
jiangyanna <jiangyanna@rong360.com>
2020-10-14 18:32:14 +0800

    add nuxt project

---
+++ nuxt_practice/pages/about.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="page-about">
+    about page
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+
+    }
+  },
+  components: {
+
+  },
+  computed: {
+
+  },
+  mounted () {
+
+  },
+  methods: {
+
+  }
+}
+</script>
+
+<style scoped lang="scss">
+
+</style>
